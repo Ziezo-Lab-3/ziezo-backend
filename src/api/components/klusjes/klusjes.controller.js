835cc490f1f17a3899072c41027363f7b752a4af
GilianS <r0885351@student.thomasmore.be>
2023-05-04 16:09:08 +0200

    feat: working implementation, no documetn count

--- src/api/components/klusjes/klusjes.controller.js
+++ src/api/components/klusjes/klusjes.controller.js
@@ -5,27 +5,81 @@ class KlusjesController {
     getKlusjeByID(req, res) {
         Klusje.findById(req.params.id, (error, data) => {
             if (error) {
-                res.status(500).send(new ApiResult("error", null, error));
+                res.status(500).send(new ApiResult("error", error));
             } else {
                 res.status(200).json(new ApiResult("success", data));
             }
         });
     }
-    
-    getKlusjes(req, res) {
-        console.log(req.query);
-        const filter = JSON.parse(req.query.filter);
-        Klusje.find(filter, (error, data) => {
-            if (error) {
-                res.status(500).send(new ApiResult("error", null, error));
-            } else {
-                res.status(200).json(new ApiResult("success", data));
+
+    async getKlusjes(req, res) {
+        try {
+            const filter = JSON.parse(req.query.filter);
+
+            // verify that first and last are both provided or both not provided
+            if (req.query.first && !req.query.last) {
+                let warning = new ApiResult(
+                    "fail",
+                    null,
+                    "last is required when first is provided"
+                );
+                res.status(400).send(warning);
+                return;
+            } else if (!req.query.first && req.query.last) {
+                const warning = new ApiResult(
+                    "fail",
+                    null,
+                    "first is required when last is provided"
+                );
+                res.status(400).send(warning);
+                return;
             }
-        });
+            let totalDocCount = 0;
+            // return all documents that match the filter (limited by first and last)
+            if (req.query.first && req.query.last) {
+                const first = parseInt(req.query.first);
+                const last = parseInt(req.query.last);
+                if (first > last) {
+                    const warning = new ApiResult(
+                        "fail",
+                        null,
+                        "first must be smaller than last"
+                    );
+                    res.status(400).send(warning);
+                    return;
+                }
+                const data = await Klusje.find(filter)
+                    .skip(first)
+                    .limit(last - first + 1)
+                    .exec();
+                res.status(200).json(
+                    new ApiResult(
+                        "success",
+                        data,
+                        `totalDocumentCount: ${totalDocCount}`
+                    )
+                );
+            } else {
+                // return all documents that match the filter (no limit)
+                Klusje.find(filter, (error, data) => {
+                    if (error) {
+                        res.status(500).send(new ApiResult("error", error));
+                    } else {
+                        res.status(200).json(
+                            new ApiResult(
+                                "success",
+                                `totalDocumentCount: ${totalDocCount}`
+                            )
+                        );
+                    }
+                });
+            }
+        } catch (error) {
+            res.status(500).send(new ApiResult("error", error.message));
+        }
     }
     async createKlusje(req, res) {
         try {
-            console.log(req.body);
             const data = new Klusje({
                 name: req.body.name,
                 description: req.body.description,
@@ -39,7 +93,7 @@ class KlusjesController {
             const savedData = await data.save();
             res.status(200).json(new ApiResult("success", savedData));
         } catch (error) {
-            res.status(500).send(new ApiResult("error", null, error));
+            res.status(500).send(new ApiResult("error", error));
         }
     }
     async updateKlusje(req, res) {
@@ -47,10 +101,14 @@ class KlusjesController {
             const id = req.params.id;
             const data = req.body;
             const options = { new: true };
-            const updatedData = await Klusje.findByIdAndUpdate(id, data, options);
+            const updatedData = await Klusje.findByIdAndUpdate(
+                id,
+                data,
+                options
+            );
             res.status(200).json(new ApiResult("success", updatedData));
         } catch (error) {
-            res.status(500).send(new ApiResult("error", null, error));
+            res.status(500).send(new ApiResult("error", error));
         }
     }
     async deleteKlusje(req, res) {
@@ -59,7 +117,7 @@ class KlusjesController {
             const deletedData = await Klusje.findByIdAndDelete(id);
             res.status(200).json(new ApiResult("success", deletedData));
         } catch (error) {
-            res.status(500).send(new ApiResult("error", null, error));
+            res.status(500).send(new ApiResult("error", error));
         }
     }
 }
