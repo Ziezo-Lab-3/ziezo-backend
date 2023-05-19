1f4e11bc45bc32ca75edf452bd4adc1283ac3744
GilianS <r0885351@student.thomasmore.be>
2023-05-19 15:22:07 +0200

    feat: only return chatgroups the user belongs to

--- src/api/components/chatgroup/chatgroup.controller.js
+++ src/api/components/chatgroup/chatgroup.controller.js
@@ -1,5 +1,6 @@
 const ChatGroup = require("./chatgroup.model");
 const { ApiResult } = require("../../JSend");
+const jwt = require("jsonwebtoken");
 
 class ChatGroupController {
     getChatGroupByID(req, res) {
@@ -12,13 +13,27 @@ class ChatGroupController {
         });
     }
     getChatGroups(req, res) {
-        ChatGroup.find(req.query, (error, data) => {
+        // use jwt to unpack the x-access-token
+        const token = req.headers["x-access-token"];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = decoded.id;
+
+        // get all chatgroups where the user is a member (chatgroup.members is an array)
+        ChatGroup.find({ members: user }, (error, data) => {
             if (error) {
                 res.status(500).send(new ApiResult("error", null, error));
             } else {
                 res.status(200).json(new ApiResult("success", data));
             }
         });
+
+        // ChatGroup.find(req.query, (error, data) => {
+        //     if (error) {
+        //         res.status(500).send(new ApiResult("error", null, error));
+        //     } else {
+        //         res.status(200).json(new ApiResult("success", data));
+        //     }
+        // });
     }
     async createChatGroup(req, res) {
         try {
