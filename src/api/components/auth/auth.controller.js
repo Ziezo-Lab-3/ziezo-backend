4cb724661cd84db1c896a3fbee2907287401362e
GilianS <r0885351@student.thomasmore.be>
2023-06-14 13:51:47 +0200

    fix: name_first and name_last correct formatting

--- src/api/components/auth/auth.controller.js
+++ src/api/components/auth/auth.controller.js
@@ -10,8 +10,8 @@ class AuthController {
         const user = new User({
             email: req.body.email,
             password: bcrypt.hashSync(req.body.password, 8),
-            name_first: req.body.firstName,
-            name_last: req.body.lastName,
+            name_first: req.body.name_first,
+            name_last: req.body.name_last,
         });
 
         user.save((err, user) => {
